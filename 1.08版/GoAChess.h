99ac244160e3051a300e230f31a2cb44cde2cd96
XuShuwen <xushuwen23@mails.ucas.ac.cn>
2025-01-30 12:17:49 +0800

    GoAChess.h

---
+++ 1.08版/GoAChess.h
@@ -0,0 +1,9 @@
+#pragma once
+#include"Gobang.h"
+//三、下棋功能实现函数
+void Human_vs_Human(void);     			//人人对战模式
+void Human_vs_AI(void);       			//人机对战模式
+void Human_Black_go();              	//人类下黑棋
+void Human_White_go();              	//人类下白棋
+void AI_Black_go(int count);            //AI下黑棋
+void AI_White_go(int count);            //AI下白棋
